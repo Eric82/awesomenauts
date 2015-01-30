90930f62229f7fab7ef741d6e81a25369310e473
Eric Martinez <martinez_eric82@yahoo.com>
2015-01-29 18:55:45 -0800

    completed video 10 and commented

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,11 +10,17 @@ game.PlayerEntity = me.Entity.extend({
                spriteheight: "64",
                getShape: function(){
                    return(new me.Rect(0, 0, 64, 64)).toPolygon();
+                   
                }
        }]);
        //this line is the speed of my player
        this.body.setVelocity(5, 20);
-   
+       
+       this.renderable.addAnimation("idle", [78]);
+       //this will animate my character while he walks.
+       this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 123, 124, 125] )
+       
+       this.renderable.setCurrentAnimation("idle");
    },
   //in update function it will check if I pressed the key so that it moves when I press it.
    update: function (delta){
@@ -22,7 +28,8 @@ game.PlayerEntity = me.Entity.extend({
           //adds to the position of my x by the velocity defined above in 
           //setVelocity() and multiplying it by me.timer.tick.
           //me.timer.tick makes the movement look smooth
-           this.body.vel.x += this.body.accel.x * me.timer.tick;   
+           this.body.vel.x += this.body.accel.x * me.timer.tick;
+           this.renderable.setCurrentAnimation("walk");
           }else{
               this.body.vel.x = 0;
           }
