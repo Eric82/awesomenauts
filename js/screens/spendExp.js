78ede3c59d24eac965e527c71cadfb66f2094bfc
Eric Martinez <martinez_eric82@yahoo.com>
2015-03-26 19:48:09 -0700

    completed video 48

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -14,7 +14,7 @@ game.SpendExp = me.ScreenObject.extend({
             
                 me.game.world.addChild(new (me.Renderable.extend({
                     init: function(){
-                        this._super(me.Renderable, 'init', [270, 240, 300, 50]);
+                        this._super(me.Renderable, 'init', [10, 300, 300, 300]);
                         this.font = new me.Font("Arial", 26, "white");
                         
                     },
